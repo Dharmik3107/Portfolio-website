3db0ea4f5b08c2af05333616ea9a73c003712c1f
Dharmik Abhangi <abhangidharmik@gmail.com>
2023-04-14 22:57:29 +0530

    Portfolio v2 phase-1 completed

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -22,9 +22,10 @@ const ContactCard = () => {
 		setFormValue({ ...formValue, [name]: value });
 	};
 
-	const handleSubmit = (event) => {
+	const handleSubmit = async (event) => {
 		event.preventDefault();
-		notify(formValue);
+		await notify(formValue);
+		setFormValue(initialFormValue);
 	};
 
 	return (
