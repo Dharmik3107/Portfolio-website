421e5715edbe6d523484c353a9ef8ff816220844
Dharmik Abhangi <abhangidharmik@gmail.com>
2023-04-28 13:42:28 +0530

    Project Data Changed

--- src/utils/projectData.js
+++ src/utils/projectData.js
@@ -1,40 +1,36 @@
 import HazguardLogo from "../assets/HazguardLogo.svg";
-import CloudyLogo from "../assets/CloudyLogo.svg";
+import StudyHelperAI from "../assets/StudyHelperAI.svg";
 import ClothiestLogo from "../assets/ClothiestLogo.svg";
 import HazguardDemo from "../assets/HazguardDemo.png";
 import CloudyDemo from "../assets/CloudyDemo.png";
 import ClothiestDemo from "../assets/ClothiestDemo.png";
 
 export const projects = [
-  {
-    logoUrl: HazguardLogo,
-    title: "Hazguard",
-    subtitle: "A hazard-sharing app - Side Project",
-    description:
-      "We at HazGuard aim to bring together companies across various industries on one page to help them share hazards and solutions that happen in their workplaces.",
-    websiteUrl: "https://www.hazguard.tech/",
-    githubUrl:
-      "https://github.com/pesto-students/hazguard-frontend-team1_sumit-srivastava",
-    demoImageUrl: HazguardDemo,
-  },
-  {
-    logoUrl: CloudyLogo,
-    title: "Cloudy",
-    subtitle: "Cloud Drive Application - Side Project",
-    description:
-      "Cloudy is cloud drive file storage application look-like google drive where user can upload files and create folders",
-    websiteUrl: "",
-    githubUrl: "https://github.com/Dharmik3107/CloudDrive-Javascript",
-    demoImageUrl: CloudyDemo,
-  },
-  {
-    logoUrl: ClothiestLogo,
-    title: "Clothiest",
-    subtitle: "E-Commerce Cloth Store - Side Project",
-    description:
-      "Clothiest is e-commerce cloth store made using ASOS API to display clothes and categorized by men and women category",
-    websiteUrl: "",
-    githubUrl: "https://github.com/Dharmik3107/clothiest-ecommorce-frontend",
-    demoImageUrl: ClothiestDemo,
-  },
+	{
+		logoUrl: HazguardLogo,
+		title: "Hazguard",
+		subtitle: "A hazard-sharing app - Side Project",
+		description: "We at HazGuard aim to bring together companies across various industries on one page to help them share hazards and solutions that happen in their workplaces.",
+		websiteUrl: "https://www.hazguard.tech/",
+		githubUrl: "https://github.com/pesto-students/hazguard-frontend-team1_sumit-srivastava",
+		demoImageUrl: HazguardDemo,
+	},
+	{
+		logoUrl: StudyHelperAI,
+		title: "Study Helper AI",
+		subtitle: "AI Based Project - Side Project (Under Development)",
+		description: "Study Helper AI uses ChatGPT AI to assist students in solving their subject-related doubts via chat.",
+		websiteUrl: "https://chat-study-helper.netlify.app/",
+		githubUrl: "https://github.com/Dharmik3107/Study-Helper-AI-frontend",
+		demoImageUrl: CloudyDemo,
+	},
+	{
+		logoUrl: ClothiestLogo,
+		title: "Clothiest",
+		subtitle: "E-Commerce Cloth Store - Side Project",
+		description: "Clothiest is e-commerce cloth store made using ASOS API to display clothes and categorized by men and women category",
+		websiteUrl: "https://clothiest.netlify.app/",
+		githubUrl: "https://github.com/Dharmik3107/clothiest-ecommorce-frontend",
+		demoImageUrl: ClothiestDemo,
+	},
 ];
